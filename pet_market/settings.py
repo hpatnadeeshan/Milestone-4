eb8b244ab0d0bb7857038877a8d70ef4ca9c4b03
Tharindu Nadeeshan <126822083+hpatnadeeshan@users.noreply.github.com>
2024-03-05 18:08:25 +0000

    removed whitenoise starage

--- pet_market/settings.py
+++ pet_market/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pet_market.urls'
@@ -164,8 +164,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_LOCATION = 'static'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_LOCATION = 'static'
 
 
 MEDIA_URL = '/media/'
