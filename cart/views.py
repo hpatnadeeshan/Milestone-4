6a1e4137ae18ddb4627f57c3a756ea959274e7ad
Tharindu Nadeeshan <126822083+hpatnadeeshan@users.noreply.github.com>
2024-03-14 21:54:02 +0000

    add delete and update items from cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 def view_cart(request):
@@ -22,3 +22,29 @@ def add_to_cart(request, item_id):
     # print(request.session['cart'])
     return redirect(redirect_url)
     
+def update_cart(request, item_id):
+    """Change the quantity of a product to the certain amount"""
+
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def remove_from_cart(request, item_id):
+    """Remove a product from the shopping cart"""
+
+    cart = request.session.get('cart', {})
+
+    if item_id in cart:
+        del cart[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
