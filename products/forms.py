49ce09605c53ea85abf4ed8b0fdc004d4c3004a5
Tharindu Nadeeshan <hpatnadeeshan@gmail.com>
2024-03-16 19:58:11 +0000

    validate the add product form  for adding images

--- products/forms.py
+++ products/forms.py
@@ -17,3 +17,13 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        image_url_first = cleaned_data.get('image_url_first')
+        image = self.cleaned_data.get('image')
+        
+        if not image_url_first and not image:
+            raise forms.ValidationError("You must provide either an image link or upload an image.")
+
+        return cleaned_data
