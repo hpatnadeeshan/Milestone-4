1f3c21f527a7652f6f70754971188362400337f2
Tharindu Nadeeshan <hpatnadeeshan@gmail.com>
2024-03-16 23:21:39 +0000

    changed the custom domain

--- pet_market/settings.py
+++ pet_market/settings.py
@@ -191,10 +191,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'pet-market-b5febc7e8269'
-    AWS_S3_REGION_NAME = 'Europe (London) eu-west-2'
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = 'http://pet-market-b5febc7e8269.s3-website.eu-west-2.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
