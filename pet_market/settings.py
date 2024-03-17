977af9dc9dd996d3bbbca2d40029bf751ad4dddf
Tharindu Nadeeshan <hpatnadeeshan@gmail.com>
2024-03-17 01:36:40 +0000

    Added cache control

--- pet_market/settings.py
+++ pet_market/settings.py
@@ -189,6 +189,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'pet-market-b5febc7e8269'
     AWS_S3_REGION_NAME = 'eu-west-2'
