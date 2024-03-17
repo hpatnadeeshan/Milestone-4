05d8be06f72fd3802088e8d57b267efd1c0da4c7
Tharindu Nadeeshan <hpatnadeeshan@gmail.com>
2024-03-17 19:55:50 +0000

    debugging modal and toast show same time

--- home/static/home/js/modal.js
+++ home/static/home/js/modal.js
@@ -1,30 +1,35 @@
 window.onload = function () {
     var modal = document.getElementById("myModal");
     var span = document.getElementsByClassName("close")[0];
+    var modalDisplayed = false;
 
     // Check if the toast message exists, if so, do not show the modal
     var toastMessage = document.querySelector('.toast');
     if (!toastMessage) {
         modal.style.display = "block";
-        var showStyle = document.createElement('style');
-        showStyle.innerHTML = '.fade:not(.show) { opacity: 1; }';
-        document.head.appendChild(showStyle);
+        modalDisplayed = true;
     }
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
-        var hideStyle = document.createElement('style');
-        hideStyle.innerHTML = '.fade:not(.show) { opacity: 0; }';
-        document.head.appendChild(hideStyle);
+        modalDisplayed = false;
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
-            hideStyle.innerHTML = '.fade:not(.show) { opacity: 0; }';
-            document.head.appendChild(hideStyle);
+            modalDisplayed = false;
         }
     }
+
+    // Function to close the modal
+    function closeModal() {
+        modal.style.display = "none";
+        modalDisplayed = false;
+    }
+
+    // Expose the closeModal function so it can be called from outside
+    window.closeModal = closeModal;
 }
