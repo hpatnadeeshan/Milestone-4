21840e9a7edb56ee8ed6a3b7cba6dde05ecfa6cf
Tharindu Nadeeshan <hpatnadeeshan@gmail.com>
2024-03-17 12:57:34 +0000

    added contactmessage to admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
+# admin.py
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'message', 'created_at']
+    search_fields = ['name', 'email', 'message']
+    list_filter = ['created_at']
+
+admin.site.register(ContactMessage, ContactMessageAdmin)
